Khan/genqlient

commit e597cac74c31b523038b1f5adc75c7b5ae47314d

add option to force using a pointer

generate/comments.go
@@ -50,7 +50,31 @@ type GenqlientDirective struct {
 	// value otherwise.
 	//
 	// Only applicable to arguments of nullable types.
-	Omitempty bool
+	Omitempty *bool
+
+	// If set, this argument or field will use a pointer type in Go.  Response
+	// types always use pointers, but otherwise we typically do not.
+	//
+	// This can be useful if it's a type you'll need to pass around (and want a
+	// pointer to save copies) or if you wish to distinguish between the Go
+	// zero value and null (for nullable fields).
+	Pointer *bool
+}
+
+func (g *GenqlientDirective) GetOmitempty() bool { return g.Omitempty != nil && *g.Omitempty }
+func (g *GenqlientDirective) GetPointer() bool   { return g.Pointer != nil && *g.Pointer }
+
+func setBool(dst **bool, v *ast.Value) error {
+	ei, err := v.Value(nil) // no vars allowed
+	// TODO: here and below, put positions on these errors
+	if err != nil {
+		return fmt.Errorf("invalid boolean value %v: %w", v, err)
+	}
+	if b, ok := ei.(bool); ok {
+		*dst = &b
+		return nil
+	}
+	return fmt.Errorf("expected boolean, got non-boolean value %T(%v)", ei, ei)
 }
 
 func fromGraphQL(dir *ast.Directive) (*GenqlientDirective, error) {
@@ -61,12 +85,19 @@ func fromGraphQL(dir *ast.Directive) (*GenqlientDirective, error) {
 	}
 
 	var retval GenqlientDirective
+	var err error
 	for _, arg := range dir.Arguments {
 		switch arg.Name {
+		// TODO: reflect and struct tags?
 		case "omitempty":
-			retval.Omitempty = true
+			err = setBool(&retval.Omitempty, arg.Value)
+		case "pointer":
+			err = setBool(&retval.Pointer, arg.Value)
 		default:
 			return nil, fmt.Errorf("unknown argument %v for @genqlient", arg.Name)
+		}
+		if err != nil {
+			return nil, err
 		}
 	}
 	return &retval, nil
@@ -79,12 +110,12 @@ func (dir *GenqlientDirective) validate(node interface{}) error {
 		// whatever it is relevant to.
 		return nil
 	case *ast.VariableDefinition:
-		if dir.Omitempty && node.Type.NonNull {
+		if dir.Omitempty != nil && node.Type.NonNull {
 			return fmt.Errorf("omitempty may only be used on optional arguments")
 		}
 		return nil
 	case *ast.Field:
-		if dir.Omitempty {
+		if dir.Omitempty != nil {
 			return fmt.Errorf("omitempty is not appilcable to fields")
 		}
 		return nil
@@ -94,11 +125,13 @@ func (dir *GenqlientDirective) validate(node interface{}) error {
 }
 
 func (dir *GenqlientDirective) merge(other *GenqlientDirective) *GenqlientDirective {
-	if dir == nil {
-		return other
+	retval := *dir
+	if other.Omitempty != nil {
+		retval.Omitempty = other.Omitempty
 	}
-	var retval GenqlientDirective
-	retval.Omitempty = dir.Omitempty || other.Omitempty
+	if other.Pointer != nil {
+		retval.Pointer = other.Pointer
+	}
 	return &retval
 }
 
@@ -106,6 +139,7 @@ func (g *generator) parsePrecedingComment(
 	node interface{},
 	pos *ast.Position,
 ) (comment string, directive *GenqlientDirective, err error) {
+	directive = new(GenqlientDirective)
 	var commentLines []string
 	sourceLines := strings.Split(pos.Src.Input, "\n")
 	for i := pos.Line - 1; i > 0; i-- {

generate/generate.go
@@ -61,7 +61,7 @@ type argument struct {
 	GoName      string
 	GoType      string
 	GraphQLName string
-	Omitempty   bool
+	Options     *GenqlientDirective
 }
 
 func newGenerator(config *Config, schema *ast.Schema) *generator {
@@ -117,14 +117,10 @@ func (g *generator) getArgument(
 	if err != nil {
 		return argument{}, err
 	}
-	directive = operationDirective.merge(directive)
-	omitempty := false
-	if directive != nil {
-		omitempty = directive.Omitempty
-	}
 
 	graphQLName := arg.Variable
-	goType, err := g.getTypeForInputType(opName, arg.Type)
+	goType, err := g.getTypeForInputType(
+		opName, arg.Type, directive, operationDirective)
 	if err != nil {
 		return argument{}, err
 	}
@@ -132,7 +128,7 @@ func (g *generator) getArgument(
 		GraphQLName: graphQLName,
 		GoName:      lowerFirst(graphQLName),
 		GoType:      goType,
-		Omitempty:   omitempty,
+		Options:     directive,
 	}, nil
 }
 
@@ -163,7 +159,7 @@ func (g *generator) addOperation(op *ast.OperationDefinition) error {
 		}
 	}
 
-	responseName, err := g.getTypeForOperation(op)
+	responseName, err := g.getTypeForOperation(op, directive)
 	if err != nil {
 		return err
 	}

generate/operation.go.tmpl
@@ -24,11 +24,13 @@ func {{.Name}}(
     {{- if .Args -}}
     variables := map[string]interface{}{
         {{range .Args -}}
-        "{{.GraphQLName}}": {{if .Omitempty}}nil{{else}}{{.GoName}}{{end}},
+        {{if not .Options.GetOmitempty -}}
+        "{{.GraphQLName}}": {{.GoName}},
+        {{end -}}
         {{end}}
     }
     {{range .Args -}}
-    {{if .Omitempty -}}
+    {{if .Options.GetOmitempty -}}
     {{/* zero_{{.GoType}} would be a better name, but {{.GoType}} would require
          munging since it might be, say, `time.Time`. */}}
     var zero_{{.GoName}} {{.GoType}}

generate/testdata/queries/Omitempty.graphql
@@ -3,9 +3,12 @@ query OmitEmptyQuery(
   $query: UserQueryInput,
   $dt: DateTime,
   $tz: String,
+  # @genqlient(omitempty: false)
+  $tzNoOmitEmpty: String,
 ) {
   user(query: $query) {
     id
   }
   maybeConvert(dt: $dt, tz: $tz)
+  convert2: maybeConvert(dt: $dt, tz: $tzNoOmitEmpty)
 }

generate/testdata/queries/Omitempty.graphql.go
@@ -12,6 +12,7 @@ import (
 type OmitEmptyQueryResponse struct {
 	User         OmitEmptyQueryUser `json:"user"`
 	MaybeConvert time.Time          `json:"maybeConvert"`
+	Convert2     time.Time          `json:"convert2"`
 }
 
 type OmitEmptyQueryUser struct {
@@ -38,26 +39,13 @@ func OmitEmptyQuery(
 	query UserQueryInput,
 	dt time.Time,
 	tz string,
+	tzNoOmitEmpty string,
 ) (*OmitEmptyQueryResponse, error) {
 	variables := map[string]interface{}{
-		"query": nil,
-		"dt":    nil,
-		"tz":    nil,
-	}
-
-	var zero_query UserQueryInput
-	if query != zero_query {
-		variables["query"] = query
-	}
-
-	var zero_dt time.Time
-	if dt != zero_dt {
-		variables["dt"] = dt
-	}
-
-	var zero_tz string
-	if tz != zero_tz {
-		variables["tz"] = tz
+		"query":         query,
+		"dt":            dt,
+		"tz":            tz,
+		"tzNoOmitEmpty": tzNoOmitEmpty,
 	}
 
 	var retval OmitEmptyQueryResponse
@@ -65,11 +53,12 @@ func OmitEmptyQuery(
 		nil,
 		"OmitEmptyQuery",
 		`
-query OmitEmptyQuery ($query: UserQueryInput, $dt: DateTime, $tz: String) {
+query OmitEmptyQuery ($query: UserQueryInput, $dt: DateTime, $tz: String, $tzNoOmitEmpty: String) {
 	user(query: $query) {
 		id
 	}
 	maybeConvert(dt: $dt, tz: $tz)
+	convert2: maybeConvert(dt: $dt, tz: $tzNoOmitEmpty)
 }
 `,
 		&retval,

generate/generate_test.go
@@ -95,8 +95,11 @@ func TestGenerate(t *testing.T) {
 				}
 
 				if string(content) != expectedContent {
-					t.Errorf("mismatch in %v\ngot:\n%v\nwant:\n%v\n",
-						filename, string(content), expectedContent)
+					t.Errorf("mismatch in %v", filename)
+					if testing.Verbose() {
+						t.Errorf("got:\n%v\nwant:\n%v\n",
+							string(content), expectedContent)
+					}
 					if update {
 						t.Log("Updating testdata dir to match")
 						err = ioutil.WriteFile(filepath.Join(dataDir, filename), content, 0o644)

generate/testdata/queries/Pointers.graphql
@@ -1,0 +1,20 @@
+# @genqlient(pointer: true)
+query PointersQuery(
+  $query: UserQueryInput,
+  # @genqlient(pointer: false)
+  $dt: DateTime,
+  $tz: String,
+) {
+  user(query: $query) {
+    id
+    roles
+    name
+    emails
+    # @genqlient(pointer: false)
+    emailsNoPtr: emails
+  }
+  otherUser: user(query: $query) {
+    id
+  }
+  maybeConvert(dt: $dt, tz: $tz)
+}

generate/testdata/queries/Pointers.graphql.go
@@ -1,0 +1,80 @@
+package test
+
+// Code generated by github.com/Khan/genqlient, DO NOT EDIT.
+
+import (
+	"time"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/me/mypkg"
+)
+
+type PointersQueryOtherUser struct {
+	Id *mypkg.ID `json:"id"`
+}
+
+type PointersQueryResponse struct {
+	User         *PointersQueryUser      `json:"user"`
+	OtherUser    *PointersQueryOtherUser `json:"otherUser"`
+	MaybeConvert *time.Time              `json:"maybeConvert"`
+}
+
+type PointersQueryUser struct {
+	Id          *mypkg.ID `json:"id"`
+	Roles       []*Role   `json:"roles"`
+	Name        *string   `json:"name"`
+	Emails      []*string `json:"emails"`
+	EmailsNoPtr []string  `json:"emailsNoPtr"`
+}
+
+type Role string
+
+const (
+	RoleStudent Role = "STUDENT"
+	RoleTeacher Role = "TEACHER"
+)
+
+type UserQueryInput struct {
+	Email *string   `json:"email"`
+	Name  *string   `json:"name"`
+	Id    *mypkg.ID `json:"id"`
+	Role  *Role     `json:"role"`
+	Names []*string `json:"names"`
+}
+
+func PointersQuery(
+	client graphql.Client,
+	query UserQueryInput,
+	dt time.Time,
+	tz string,
+) (*PointersQueryResponse, error) {
+	variables := map[string]interface{}{
+		"query": query,
+		"dt":    dt,
+		"tz":    tz,
+	}
+
+	var retval PointersQueryResponse
+	err := client.MakeRequest(
+		nil,
+		"PointersQuery",
+		`
+query PointersQuery ($query: UserQueryInput, $dt: DateTime, $tz: String) {
+	user(query: $query) {
+		id
+		roles
+		name
+		emails
+		emailsNoPtr: emails
+	}
+	otherUser: user(query: $query) {
+		id
+	}
+	maybeConvert(dt: $dt, tz: $tz)
+}
+`,
+		&retval,
+		variables,
+	)
+	return &retval, err
+}

commit c2e7dc4e5b12c834054df106de0527055b204b38

Handle omitempty correctly for slices
We were generating broken code; fixes #43.  Also fixes a bug where
applying omitempty to the entire query was broken by e597cac74c3.

generate/comments.go
@@ -43,7 +43,9 @@ type GenqlientDirective struct {
 	pos *ast.Position
 
 	// If set, this argument will be omitted if it's equal to its Go zero
-	// value.  For example, given the following query:
+	// value, or is an empty slice.
+	//
+	// For example, given the following query:
 	//	# @genqlient(omitempty: true)
 	//	query MyQuery(arg: String) { ... }
 	// genqlient will generate a function

generate/generate.go
@@ -60,6 +60,7 @@ type argument struct {
 	GoName      string
 	GoType      string
 	GraphQLName string
+	IsSlice     bool
 	Options     *GenqlientDirective
 }
 
@@ -127,7 +128,8 @@ func (g *generator) getArgument(
 		GraphQLName: graphQLName,
 		GoName:      lowerFirst(graphQLName),
 		GoType:      goType,
-		Options:     directive,
+		IsSlice:     arg.Type.Elem != nil,
+		Options:     operationDirective.merge(directive),
 	}, nil
 }
 

generate/operation.go.tmpl
@@ -31,10 +31,14 @@ func {{.Name}}(
     }
     {{range .Args -}}
     {{if .Options.GetOmitempty -}}
+    {{if .IsSlice -}}
+    if len({{.GoName}}) > 0 {
+    {{else -}}
     {{/* zero_{{.GoType}} would be a better name, but {{.GoType}} would require
          munging since it might be, say, `time.Time`. */}}
     var zero_{{.GoName}} {{.GoType}}
     if {{.GoName}} != zero_{{.GoName}} {
+    {{end -}}
         variables["{{.GraphQLName}}"] = {{.GoName}}
     }
     {{end}}

generate/testdata/queries/Omitempty.graphql
@@ -1,14 +1,14 @@
 # @genqlient(omitempty: true)
 query OmitEmptyQuery(
   $query: UserQueryInput,
+  $queries: [UserQueryInput],
   $dt: DateTime,
   $tz: String,
   # @genqlient(omitempty: false)
   $tzNoOmitEmpty: String,
 ) {
-  user(query: $query) {
-    id
-  }
+  user(query: $query) { id }
+  users(query: $queries) { id }
   maybeConvert(dt: $dt, tz: $tz)
   convert2: maybeConvert(dt: $dt, tz: $tzNoOmitEmpty)
 }

generate/testdata/queries/Omitempty.graphql.go
@@ -15,9 +15,10 @@ type OmitEmptyQueryResponse struct {
 	//
 	// See UserQueryInput for what stuff is supported.
 	// If query is null, returns the current user.
-	User         OmitEmptyQueryUser `json:"user"`
-	MaybeConvert time.Time          `json:"maybeConvert"`
-	Convert2     time.Time          `json:"convert2"`
+	User         OmitEmptyQueryUser      `json:"user"`
+	Users        OmitEmptyQueryUsersUser `json:"users"`
+	MaybeConvert time.Time               `json:"maybeConvert"`
+	Convert2     time.Time               `json:"convert2"`
 }
 
 // OmitEmptyQueryUser includes the requested fields of the GraphQL type User.
@@ -25,6 +26,17 @@ type OmitEmptyQueryResponse struct {
 //
 // A User is a user!
 type OmitEmptyQueryUser struct {
+	// id is the user's ID.
+	//
+	// It is stable, unique, and opaque, like all good IDs.
+	Id mypkg.ID `json:"id"`
+}
+
+// OmitEmptyQueryUsersUser includes the requested fields of the GraphQL type User.
+// The GraphQL type's documentation follows.
+//
+// A User is a user!
+type OmitEmptyQueryUsersUser struct {
 	// id is the user's ID.
 	//
 	// It is stable, unique, and opaque, like all good IDs.
@@ -62,15 +74,32 @@ type UserQueryInput struct {
 func OmitEmptyQuery(
 	client graphql.Client,
 	query UserQueryInput,
+	queries []UserQueryInput,
 	dt time.Time,
 	tz string,
 	tzNoOmitEmpty string,
 ) (*OmitEmptyQueryResponse, error) {
 	variables := map[string]interface{}{
-		"query":         query,
-		"dt":            dt,
-		"tz":            tz,
 		"tzNoOmitEmpty": tzNoOmitEmpty,
+	}
+
+	var zero_query UserQueryInput
+	if query != zero_query {
+		variables["query"] = query
+	}
+
+	if len(queries) > 0 {
+		variables["queries"] = queries
+	}
+
+	var zero_dt time.Time
+	if dt != zero_dt {
+		variables["dt"] = dt
+	}
+
+	var zero_tz string
+	if tz != zero_tz {
+		variables["tz"] = tz
 	}
 
 	var retval OmitEmptyQueryResponse
@@ -78,8 +107,11 @@ func OmitEmptyQuery(
 		nil,
 		"OmitEmptyQuery",
 		`
-query OmitEmptyQuery ($query: UserQueryInput, $dt: DateTime, $tz: String, $tzNoOmitEmpty: String) {
+query OmitEmptyQuery ($query: UserQueryInput, $queries: [UserQueryInput], $dt: DateTime, $tz: String, $tzNoOmitEmpty: String) {
 	user(query: $query) {
+		id
+	}
+	users(query: $queries) {
 		id
 	}
 	maybeConvert(dt: $dt, tz: $tz)

generate/testdata/queries/schema.graphql
@@ -99,6 +99,8 @@ type Query {
   """
   user(query: UserQueryInput): User
 
+  users(query: [UserQueryInput]): User
+
   """usersWithRole looks a user up by role."""
   usersWithRole(role: Role!): [User!]!
   root: Topic!